b32d3281791fcf63be3e51a3a2e4e2cd92d53a0e
Manuel Peuster <manuel@peuster.de>
2017-05-05 14:47:46 +0200

    Try to get OSM SO and UI to work

--- upb-emulator-mano-integration-demo/scripts/install_osm.sh
+++ upb-emulator-mano-integration-demo/scripts/install_osm.sh
@@ -13,4 +13,17 @@ cd /home/vagrant
 git clone https://osm.etsi.org/gerrit/osm/SO.git
 cd /home/vagrant/SO
 git checkout v2.0
-#sudo ./BUILD.sh
+sudo ./BUILD.sh
+#make -j2
+sudo make install
+
+# UI
+cd /home/vagrant
+git clone https://osm.etsi.org/gerrit/osm/UI.git
+cd /home/vagrant/UI
+git checkout v2.0
+make -j2
+sudo make install
+
+# start
+# sudo -H /usr/rift/rift-shell -r -i /usr/rift -a /usr/rift/.artifacts -- ./demos/launchpad.py --use-xml-mode --test-name "launchpad"
