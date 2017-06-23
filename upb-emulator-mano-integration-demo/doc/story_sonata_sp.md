9b869d2d2766f5967f11e1e6812af82b6cc459fd
Manuel Peuster <manuel@peuster.de>
2017-06-23 13:50:55 +0200

    Cleanup, start of SONATA SP story doc, renamed SONATA SP VNFs.

---
+++ upb-emulator-mano-integration-demo/doc/story_sonata_sp.md
@@ -0,0 +1,16 @@
+#  Story: SONATA service platform ontop of emulation
+
+## Environment setup
+
+### IA only deployment
+```
+# start databases 
+~/son-tutorials/upb-emulator-mano-integration-demo/sonata-sp/deploy_environment.sh
+
+```
+
+### Full SP deployment
+
+## Demo storyboard
+
+TODO
--- upb-emulator-mano-integration-demo/vnfs/build.sh
+++ upb-emulator-mano-integration-demo/vnfs/build.sh
@@ -14,6 +14,6 @@ docker build -t http-nginx-img -f p2-nginx/Dockerfile p2-nginx
 docker build -t proxy-squid-img -f p2-squid/Dockerfile p2-squid
 
 # builds for sonata integration
-docker build -t sonata-apache -f p2-apache/Dockerfile p2-apache
-docker build -t sonata-squid -f p2-squid/Dockerfile p2-squid
-docker build -t sonata-socat -f p2-l4fw-socat/Dockerfile p2-l4fw-socat
+docker build -t eu.sonata-nfv_apache-vnf_0.1_1 -f p2-apache/Dockerfile p2-apache
+docker build -t eu.sonata-nfv_squid-vnf_0.1_1 -f p2-squid/Dockerfile p2-squid
+docker build -t eu.sonata-nfv_socat-vnf_0.1_1 -f p2-l4fw-socat/Dockerfile p2-l4fw-socat
