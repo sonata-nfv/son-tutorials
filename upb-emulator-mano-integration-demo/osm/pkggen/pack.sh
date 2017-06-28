498c81a023d171ebb2c3c1d4c08e9aee70253917
Manuel Peuster <manuel@peuster.de>
2017-06-28 19:40:57 +0200

    added packages and route file for osm

--- upb-emulator-mano-integration-demo/osm/pkggen/pack.sh
+++ upb-emulator-mano-integration-demo/osm/pkggen/pack.sh
@@ -3,3 +3,5 @@
 ./generate_descriptor_pkg.sh -t vnfd -N http
 ./generate_descriptor_pkg.sh -t vnfd -N l4fw
 ./generate_descriptor_pkg.sh -t vnfd -N proxy
+
+./generate_descriptor_pkg.sh -t nsd -N demo_nsd
---
+++ upb-emulator-mano-integration-demo/osm/set_route.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo route add -net 10.87.78.0/24 gw 10.115.16.31
+
